7361948c8515d7a50635762ee3f57f420d8d2ce7
agw <agw@billigvvs.dk>
2019-02-01 09:18:33 +0100

    updated to accept parameters instead of runtime dialog

--- functions/pull_master.fish
+++ functions/pull_master.fish
@@ -1,31 +1,40 @@
 function pull_master
+	set -l options 'h/help' 'r/restart' 'R/rebuild' 'x-excludeflex' 'y-excludeshop' 'k-excludecrawler'
+	argparse $options -- $argv ^/dev/null;
+	or begin 
+	echo "Some arguments [" $argv "] was not understood" 
+	and pull_master.help;and return 1;end
+
 	set home '/home/agw/'
 	set devenv $home'PycharmProjects/devenv/'
-	set crawl $home'billigvvs_crawlers/crawler'
-	set vvsshop $devenv'shop-base/vvsshop'
-	set neoflex $devenv'flex-base/neoflex'
+	if test -n "$_flag_h";pull_master.help;return 0;end
 
-	echo $vvsshop
-	command git -C $vvsshop checkout master
-	command git -C $vvsshop pull --all
-	
-	echo $neoflex
-	command git -C $neoflex checkout master
-	command git -C $neoflex pull --all
+	if test -z "$_flag_excludeshop"
+		set vvsshop $devenv'shop-base/vvsshop'
+		echo $vvsshop
+		command git -C $vvsshop checkout master
+		command git -C $vvsshop pull --all
+	end	
 
-	echo $crawl
-	command git -C $crawl checkout master
-	command git -C $crawl pull --all
+	if test -z "$_flag_excludeflex"
+		set neoflex $devenv'flex-base/neoflex'
+		echo $neoflex
+		command git -C $neoflex checkout master
+		command git -C $neoflex pull --all
+	end
 
-	echo 'You wanna rebuild?'
-	if read_confirm
+	if test -z "$_flag_excludecrawler"
+		set crawl $home'billigvvs_crawlers/crawler'
+		echo $crawl
+		command git -C $crawl checkout master
+		command git -C $crawl pull --all
+	end
+
+	if test -n "$_flag_r"
 		command make -C $devenv docker-rebuild
-	else
-		echo 'Restart then?'
-		if read_confirm
-			command make -C $devenv docker-restart
-		else
-			echo 'Not doing anything then'
-		end
+		return
+	end
+	if test -n "$_flag_R"
+		command make -C $devenv docker-restart
 	end
 end
