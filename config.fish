7af95aed3e4ec1bbf666dd2a2de5e7179ac6bbca
Asger Weirsøe <agw@billigvvs.dk>
2019-04-11 11:29:22 +0200

    Added miniconda3

--- config.fish
+++ config.fish
@@ -2,3 +2,4 @@ eval (python3 -m virtualfish)
 . /usr/share/autojump/autojump.fish
 set -x GOPATH $HOME/work
 set -x PATH $PATH:/usr/local/go/bin:$GOPATH/bin
+set -x PATH $PATH:/home/$USER/miniconda3/bin
